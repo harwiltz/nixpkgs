685275e77f03c84a0c167a4ec97624c72518c23e
Harley Wiltzer <harley.wiltzer@mail.mcgill.ca>
2021-12-22 14:28:16 -0500

    python3Packages.dm_haiku: init at 0.0.5 python3Packages.optax   : init at 0.1.0 python3Packages.jmp     : init at 0.0.2 python3Packages.chex    : init at 0.1.0

---
+++ pkgs/development/python-modules/chex/default.nix
@@ -0,0 +1,39 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, numpy
+, absl-py
+, toolz
+, dm-tree
+, jax
+, jaxlib
+}:
+
+buildPythonPackage rec {
+  pname = "chex";
+  version = "0.1.0";
+
+  src = fetchFromGitHub {
+    owner = "deepmind";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "sha256-V8zQ9PWLvW+sG1K6893uX62XFx8l+pB7AOqBFZd+Y0Q=";
+  };
+
+  propagatedBuildInputs = [
+    absl-py
+    jax
+    jaxlib
+    dm-tree
+    toolz
+  ];
+
+  pythonImportsCheck = [ "chex" ];
+
+  meta = with lib; {
+    description = "Chex is a library of utilities for helping to write JAX code.";
+    homepage = "https://chex.readthedocs.io";
+    license = licenses.asl20;
+    maintainers = with maintainers; [ harwiltz ];
+  };
+}
